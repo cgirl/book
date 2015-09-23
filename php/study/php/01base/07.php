b5617ead5dae39cf5beae3d55742e10d0713fd9a
cgirl <1579090779@qq.com>
2015-09-23 11:02:00 +0800

    文件管理系统

--- php/study/php/01base/07.php
+++ php/study/php/01base/07.php
@@ -13,3 +13,69 @@ if((0.3-0.2) == 0.1){
     echo 0.3-0.2, ' 不相等';
 }
 ?>
+
+<?php 
+$path = '.';
+$url = $_SERVER['REQUEST_URI'];
+
+if (isset($_GET['dir'])){
+    $path = $path.'/'.$_GET['dir'];
+} else {
+    $url = $url.'?dir=';
+}
+
+$dh = opendir($path);
+if ($dh === false){
+    echo '打开出错';
+    exit;
+}
+
+while(($item = readdir($dh)) !== false){
+    $list[] = $item;
+}
+
+print_r($list);
+closedir($dh);
+
+?>
+<!--  文件管理系统练习  -->
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-sacle=1.0">
+<title>文件管理系统</title>
+<style type="text/css">
+    td{
+    	border:1px solid gray;
+    }
+</style>
+</head>
+<body>
+	<h4>文件管理系统</h4>
+	<table>
+	   <tr>
+	       <td>序号</td>
+	       <td>文件名</td>
+	       <td>操作</td>
+	   </tr>
+	   <?php 
+	       foreach ($list as $k=>$v){
+	   ?>
+	   <tr>
+	       <td><?php echo $k ?></td>
+	       <td><?php echo $v ?></td>
+	       <td>
+	       <?php 
+	           if (is_dir($path.'/'.$v)){
+	               echo '<a href="',$url.$v,'/">浏览</a>';
+	           } else {
+	               echo '<a href="', $_GET[dir].$v, '">查看</a>';;
+	           }
+	       ?>
+	       </td>
+	   </tr>
+	   <?php }?>
+	</table>
+</body>
+</html>
